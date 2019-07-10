2f9295cc4c53a998f9024cc16362dff3f3cd9216
caioeverest <caioeverest.b@gmail.com>
2019-07-10 02:50:22 -0300

    Fix snackbar wraper

--- ui/src/stories/index.js
+++ ui/src/stories/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-knobs/register';
-
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, number, select, boolean } from '@storybook/addon-knobs';
 
-import Snacks from '../components/Atoms/Snacks';
+//Atoms
+import Snackbar from '../components/Atoms/Snackbar';
 
+//Templates
 import EventList from '../components/Templates/EventList';
 import ContactList from '../components/Templates/ContactList';
 
+//Pages
+/*TODO*/
+
 storiesOf('Atoms', module)
     .addDecorator(withKnobs)
-    .add('Snacks', () => <Snacks
+    .add('Snackbar', () => <Snackbar
         message={text('Some message', 'Hey something happens')}
-        open={boolean('it is open', false)}
-        timeout={number('Time that it will be shown um ms', 5)}
-        status={select('status', {
+        open={boolean("Open:", false)}
+        variant={select('status', {
             Success: 'success',
             Warning: 'warning',
             Error: 'error',
@@ -25,5 +28,9 @@ storiesOf('Atoms', module)
     />);
 
 storiesOf('Templates', module)
+    .add('EventList', () => <EventList />)
+    .add('ContactList', () => <ContactList name="test"/>);
+
+storiesOf('Pages', module)
     .add('EventList', () => <EventList />)
     .add('ContactList', () => <ContactList name="test"/>);
