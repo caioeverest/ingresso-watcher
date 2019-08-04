6ad6a5382f72c00371ddda758605bf6b75e6881d
caioeverest <caioeverest.b@gmail.com>
2019-08-04 17:08:16 -0300

    fix link of event on message

--- service/monitor.go
+++ service/monitor.go
@@ -25,7 +25,7 @@ func monitorDeEventos(contactList, eventList repository.Interface, whatsapp clie
 		_, err := CheckIfHaveTickets(conf, event.Id)
 		if err == nil {
 			contacts := GetContactList(contactList)
-			url := fmt.Sprintf("https://www.ingressorapido.com.br/event/%s-1", event.Id)
+			url := fmt.Sprintf("https://www.ingressorapido.com.br/event/%s", event.Id)
 			for _, contact := range contacts {
 				if err := SendFoundTicketMessage(whatsapp, contact.Phone, contact.Name, event.Name, url); err != nil {
 					log.Printf("Ocorreu um erro %s ao enviar a notificação para %s - %s", err, contact.Phone, contact.Name)
